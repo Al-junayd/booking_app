b4ca8babb9f873cb365bf22f7902f6659f478f4d
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2022-11-18 22:38:02 +0100

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <span>Testing</span>
+        <div>
+          <button>Register</button>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/PropertyListings.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PropertyListings = () => {
+  return <div>PropertyListings</div>;
+};
+
+export default PropertyListings;
